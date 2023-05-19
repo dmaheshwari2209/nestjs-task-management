8a811dd961bf2305c830052055079e2cdcdf416f
dmaheshwari2209 <100116359+dmaheshwari2209@users.noreply.github.com>
2023-05-19 21:52:57 +0530

    get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -22,4 +22,8 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
+
+  public getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
 }
